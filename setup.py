895f3f8d7b25bff7b2dd130ea377e2886881e62d
Stefan Zimmermann <user@zimmermann.co>
2019-08-12 00:04:12 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 dist = None
 try:
     dist = setup(
-        setup_requires=["zetup >= 0.2.58"],
+        setup_requires=["zetup >= 0.2.60"],
 
         use_zetup=True,
     )
